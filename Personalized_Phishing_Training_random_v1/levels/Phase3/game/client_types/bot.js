a2119061f055b649983acd722c799d98674cdb72
Edward Cranford <92046248+ecranfor@users.noreply.github.com>
2023-03-27 12:27:06 -0400

    updated bot

--- Personalized_Phishing_Training_random_v1/levels/Phase3/game/client_types/bot.js
+++ Personalized_Phishing_Training_random_v1/levels/Phase3/game/client_types/bot.js
@@ -14,27 +14,88 @@ module.exports = function(treatmentName, settings, stager,
         node.timer.random.done();
     });
 
-    stager.extendStep('game', {
-        roles: {
-            DICTATOR: {
-                cb: function() {
-                    // This Dictator BOT makes an initial offer of 50
-                    // or repeats any offer previously received.
-                    node.timer.random(3000).done({
-                        offer: 'undefined' === typeof node.game.offer ?
-                                50 : node.game.offer
-                    });
+    stager.extendStep('phase 2', {
+        cb: function() {
+            ///first trying to set values of the widget...but lastAppended is returning 'undefined'
+            //node.widgets.lastAppended.setValues({ classification: {choice: 1, value: 'No', isCorrect: true}, confidence: {value: 0, noChange: false, isCorrect: true} });
+            ///mabye instead...
+            ///need to set message of done to edit 'data' object...
+            ///so I can get data.forms.classification.choice and data.forms.confidence.value and times for each
+            node.on.data('averages', function(msg) {
+                let email_type = msg.data[0];
+                console.log("Bot email type is: "+email_type);
+                let avg_phish_acc = msg.data[1];
+                console.log("Average Phish accuracy is: "+avg_phish_acc);
+                let avg_ham_acc = msg.data[2];
+                console.log("Average Ham accuracy is: "+avg_ham_acc);
+
+                //set bot's choice based on average performance of all players...including bots
+                let bot_choice;
+                //set choice_value to 'yes' for PHISHING or 'no' for HAM
+                let choice_value;
+
+                if (email_type == 'PHISHING') {
+                    if (Math.random() <= avg_phish_acc) {
+                        bot_choice = 0;
+                        choice_value = 'yes'
+                    } else {
+                        bot_choice = 1;
+                        choice_value = 'no'
+                    }
+                } else {
+                    if (Math.random() <= avg_ham_acc) {
+                        bot_choice = 1;
+                        choice_value = 'no'
+                    } else {
+                        bot_choice = 0;
+                        choice_value = 'yes'
+                    }
+                };
+
+                if (bot_choice == 0) {
+                    console.log("Bot classification is: "+bot_choice+" "+choice_value+" (PHISHING)");
+                } else {
+                    console.log("Bot classification is: "+bot_choice+" "+choice_value+" (HAM)");
                 }
-            },
-            OBSERVER: {
-                cb: function() {
-                    node.on.data('decision', function(msg) {
-                        // Store last offer.
-                        node.game.offer = msg.data;
-                        node.timer.random(3000).done();
-                    });
-                }
-            }
+
+                node.timer.random(5000).done({
+                    order: [ 0, 1 ],
+                    forms: { 
+                        classification: {
+                            id: 'classification',
+                            choice: bot_choice,
+                            time: 0,
+                            nClicks: 1,
+                            value: choice_value,
+                            isCorrect: true,
+                            attempts: []
+                        },
+                        confidence: {
+                            value: 0,
+                            noChange: false,
+                            initialValue: 0,
+                            totalMove: 0,
+                            isCorrect: true,
+                            time: 0
+                        }},
+                    missValues: [],
+                    id: 'phase 2',
+                    isCorrect: true
+                });
+            });
         }
     });
+
+    stager.extendStep('feedback', {
+        cb: function() {
+            node.timer.random(2000).done();
+        }
+    });
+
+    stager.extendStep('phase 2 feedback', {
+        cb: function() {
+            node.timer.random(2000).done();
+        }
+    });
+
 };
